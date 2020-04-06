d982d0a4869ee1b2c5271f0f23db6c621024e68e
dirtbird <bassmanbiff@gmail.com>
2020-04-06 18:55:11 -0500

    fix setup

--- setup.sh
+++ setup.sh
@@ -2,9 +2,6 @@
 REMOTE_URL="https://github.com/dirtbirb/"
 MODULE_DIR="modules/"
 
-apt-get install -y -q $(awk '{print $1'} requirements.txt)
-
-# git submodule add ${REMOTE_URL}labgui-gui ${MODULE_DIR}gui
-# git submodule add ${REMOTE_URL}labgui-hardware ${MODULE_DIR}hardware
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+apt-get install -y $(awk '{print $1'} requirements.txt)
 git submodule update --init ${MODULE_DIR}*
-# git submodule update --init ${MODULE_DIR}hardware
