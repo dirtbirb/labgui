43085ef649ed9e1611fa79cfb5d260faef2ae559
dirtbird <bassmanbiff@gmail.com>
2020-04-06 18:16:53 -0500

    add setup script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+
+REMOTE_URL="https://github.com/dirtbirb/"
+MODULE_DIR="modules/"
+
+apt-get install -y $(awk '{print $1'} requirements.txt)
+
+git submodule add ${REMOTE_URL}labgui-gui ${MODULE_DIR}gui
+git submodule add ${REMOTE_URL}labgui-hardware ${MODULE_DIR}hardware
+# git submodule update --init ${MODULE_DIR}gui
+# git submodule update --init ${MODULE_DIR}hardware
