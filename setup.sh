5f821fd2c689f649cc9a6752da1b621255894cc3
dirtbird <bassmanbiff@gmail.com>
2020-04-06 18:47:06 -0500

    fix submodule init

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ MODULE_DIR="modules/"
 
 sudo apt-get install -y -q $(awk '{print $1'} requirements.txt)
 
-git submodule add ${REMOTE_URL}labgui-gui ${MODULE_DIR}gui
-git submodule add ${REMOTE_URL}labgui-hardware ${MODULE_DIR}hardware
-# git submodule update --init ${MODULE_DIR}gui
+# git submodule add ${REMOTE_URL}labgui-gui ${MODULE_DIR}gui
+# git submodule add ${REMOTE_URL}labgui-hardware ${MODULE_DIR}hardware
+git submodule update --init ${MODULE_DIR}*
 # git submodule update --init ${MODULE_DIR}hardware
