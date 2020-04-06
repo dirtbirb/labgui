de1bf1ad48a270fe957a5700684479d9f5eba200
dirtbird <bassmanbiff@gmail.com>
2020-04-06 18:26:14 -0500

    edit setup, gitignore

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 REMOTE_URL="https://github.com/dirtbirb/"
 MODULE_DIR="modules/"
 
-apt-get install -y $(awk '{print $1'} requirements.txt)
+apt-get install -y -q $(awk '{print $1'} requirements.txt)
 
 git submodule add ${REMOTE_URL}labgui-gui ${MODULE_DIR}gui
 git submodule add ${REMOTE_URL}labgui-hardware ${MODULE_DIR}hardware
